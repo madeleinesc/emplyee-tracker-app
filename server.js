693637c384b3bb8e39834c7600fd3476ed1d60d1
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-27 20:12:03 +1030

    created db connection

--- server.js
+++ server.js
@@ -5,5 +5,25 @@ const mysql = require('mysql2');
 // import inquirer package
 const inquirer = require('inquirer');
 // import console.table package
-const console = require('console.table');
+require('console.table');
+
+
+// create connection to database
+const newConnect = mysql.createConnection(
+    {
+      host: 'localhost',
+      
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME
+    }
+  );
+
+// async function/user prompts?
+
+
+
+// module export
+
+
 
