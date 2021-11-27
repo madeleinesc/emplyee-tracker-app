9ee15b96a91ac6303ed9d7243f734eb6d132cab6
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-27 19:28:43 +1030

    added dependencies to server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+require('dotenv').config(); // for .env
+// Import mysql2 package
+const mysql = require('mysql2');
+// import inquirer package
+const inquirer = require('inquirer');
+// import console.table package
+const console = require('console.table');
+
