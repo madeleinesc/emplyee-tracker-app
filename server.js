65c90264dd71e34bdf0d61860166e81c25004a06
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-28 17:37:53 +1030

    first attempt at starting prompts

--- server.js
+++ server.js
@@ -9,22 +9,64 @@ require('console.table');
 
 
 // create connection to database
-const newConnect = mysql.createConnection(
-    {
-      host: 'localhost',
-      
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME
-    }
-  );
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
 
-// async function/user prompts?
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+  },
+  console.log(
+    `Connected to employee database.`
+  )
+);
 
+
+// async function/user prompts
+
+
+// initialise app
+async function init() {
+
+  // start menu
+  start = () => {
+    return inquirer.prompt([
+      {
+        type: 'list',
+        name: 'menu',
+        message: "Welcome to the Employee Tracker App! Please choose from the following options:",
+        choices: [
+          'View all departments',
+          'View all roles',
+          'View all employees',
+          'Add a department',
+          'Add a role',
+          'Add an employee',
+          'Update an employees role',
+          'Exit'
+        ]
+      }
+    ])
+
+    
+};
+
+// queries
+
+
+
+}
+init();
 
 
 
 // module export
+
+
+
+
+
 
 
 
