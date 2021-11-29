d448f06f42b8910aa7f7451adebe5417f257d577
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-29 12:53:43 +1030

    attempt at third query

--- db/query.sql
+++ db/query.sql
@@ -1,13 +1,24 @@
 USE employee_db
 
-SELECT departments.dpt_name AS Department, departments.id AS DepartmentId
-FROM departments
+SELECT departments.id AS DepartmentId, departments.dpt_name AS Department
+FROM departments;
 
 
 
-SELECT roles.title AS Title, roles.id AS RoleId, roles.salary AS Salary, departments.dpt_name AS Department 
+SELECT roles.id AS RoleId, roles.title AS Title,  roles.salary AS Salary, departments.dpt_name AS Department
 FROM roles
-JOIN departments ON departments.id = role.departments_id
+JOIN departments 
+ON roles.dpt_id = departments.id;
+
+
+SELECT employees.id AS EmployeeId, employees.first_name AS FirstName, employees.last_name AS LastName, roles.title AS Title, roles.salary AS Salary, departments.dpt_name AS Department
+FROM employees
+JOIN roles
+ON employees.role_id = roles.id
+JOIN departments
+ON roles.dpt_id = departments.id
+ORDER BY employees.id;
+
 
 
 
